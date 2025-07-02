8acb3c65353df0ffc3ec534d21b87db316728e9e
silasdani <danielsilas23@yahoo.com>
2025-07-03 02:28:28 +0300

    update user serializer to use rails_blob_path for image URLs

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
   attributes :id, :email, :name, :nickname, :role, :created_at, :updated_at
 
   attribute :image_url, if: :image_attached?
+
   def image_url
-    url_for(object.image)
+    Rails.application.routes.url_helpers.rails_blob_path(object.image, only_path: true)
   end
 
   def image_attached?
