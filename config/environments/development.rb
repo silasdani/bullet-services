2a1bb7cc1dd4bf3cf9fa5a1e5f7001b918592605
silasdani <danielsilas23@yahoo.com>
2025-07-02 18:27:13 +0300

    update action mailer configuration to use environment variable for default URL options; disable mail delivery in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = false
 
   # Make template changes take effect immediately.
