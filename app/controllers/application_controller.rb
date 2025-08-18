67b6d8687dc7fcae547641ff72f1038e938b06bc
silasdani <danielsilas23@yahoo.com>
2025-08-18 15:30:31 +0300

    fix:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  # Skip CSRF verification for Devise Token Auth routes
+  skip_before_action :verify_authenticity_token, if: :auth_request?
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
   def user_not_authorized(exception)
     render json: { error: exception.policy.class.to_s.underscore + "." + exception.query }, status: :forbidden
+  end
+
+  def auth_request?
+    request.path.start_with?('/auth')
   end
 end
